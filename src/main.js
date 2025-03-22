060e2b0b73ff0056e68b32ca9b1ec67cbdd65db1
abhay333d <abhaydandge333@gmail.com>
2025-03-23 02:33:59 +0530

    testing

--- src/main.js
+++ src/main.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
           const modelHeight = bbox.max.y - bbox.min.y;
 
           // Adjust y-position to place the base on the ring
-          model.position.y += bbox.min.y;
+          model.position.y += bbox.min.y/2;
 
           scene.add(model);
         },
